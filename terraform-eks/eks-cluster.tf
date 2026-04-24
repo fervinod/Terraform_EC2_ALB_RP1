fa66c2c3aede094a76f43438623df1ee7d4f44ee
fervinod <vinodfernandes@gmail.com>
2026-04-24 15:13:27 +0800

    Update eks-cluster.tf

--- terraform-eks/eks-cluster.tf
+++ terraform-eks/eks-cluster.tf
@@ -2,7 +2,9 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         =   "21.18.0"  #"20.8.4"  # used updated version 
   name    = local.cluster_name
-  version = var.kubernetes_version
+  #version = var.kubernetes_version
+  kubernetes_version  = var.kubernetes_version
+
   subnet_ids      = module.vpc.private_subnets
 
   enable_irsa = true
