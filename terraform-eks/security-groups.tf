7271044ad1d7c79fdcd24151dce0b830a44ee747
fervinod <vinodfernandes@gmail.com>
2026-04-25 11:09:06 +0800

    Update security-groups.tf

--- terraform-eks/security-groups.tf
+++ terraform-eks/security-groups.tf
@@ -4,11 +4,11 @@ resource "aws_security_group" "all_worker_mgmt" {
   vpc_id      = module.vpc.vpc_id
 }
 
-resource "aws_security_group_rule" "all_worker_mgmt_ingress" {
-  description       = "allow all inbound traffic"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
+#resource "aws_security_group_rule" "all_worker_mgmt_ingress" {
+  #description       = "allow all inbound traffic"
+  #from_port         = 0
+ # to_port           = 0
+#  protocol          = "-1"
 
   #security_group_id = aws_security_group.all_worker_mgmt.id
   #type              = "ingress"
